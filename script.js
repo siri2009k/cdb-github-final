9fd194a80cf6fef943925352c722d1d1a486deed
Siri Kelshikar <sirikelshikar@Siris-MacBook-Air-3.local>
2023-04-01 22:06:32 -0700

    finalcdb

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 
 // button, word, containers
 var button = document.getElementById('button');
+var buttonclass = document.getElementsByClassName('button2');
 var word = document.getElementById('opening');
-var firstcloudcontainer = document.getElementById('firstcloudcontainer')
-var openingcontainer = document.getElementById('openingcontainer')
-var closing = document.getElementById('closingcontainer')
+var firstcloudcontainer = document.getElementById('firstcloudcontainer');
+var openingcontainer = document.getElementById('openingcontainer');
+var closing = document.getElementById('closingcontainer');
 
 
 //first cloud words
 var firstcloud = document.getElementById('firstCloud');
-var smallwords = document.getElementsByClassName('small')
-var mediumwords = document.getElementsByClassName('medium')
-var largewords = document.getElementsByClassName('large')
-var xlwords = document.getElementsByClassName('extraLarge')
-var closing = document.getElementById('closing')
-var closingcontainer = document.getElementById('closingcontainer')
-var firstCloudElement = document.getElementById("firstCloud")
-var firstCloudList = document.getElementsByTagName("li")
+var smallwords = document.getElementsByClassName('small');
+var mediumwords = document.getElementsByClassName('medium');
+var largewords = document.getElementsByClassName('large');
+var xlwords = document.getElementsByClassName('extraLarge');
+var closing = document.getElementById('closing');
+var closingcontainer = document.getElementById('closingcontainer');
+var firstCloudElement = document.getElementById("firstCloud");
+var firstCloudList = document.getElementsByTagName("li");
 
 //if button is pressed
 function pressed2() {
     console.log("pressed")
     firstcloud.classList.add('fadeout');
     button.classList.add('fadeout');
-    
+     
     function closings() {
     closing.classList.add('fadein')
     closing.style.display = "block";
     closingcontainer.style.placeItems = 'center';
     }
-    setTimeout(closings,500)
+    setTimeout(closings,7500)
 }
 
-
+function fadeinbutton() {
+  button.style.display = "block";
+  button.classList.add('fadeinbutton')
+ 
+}
 //switching from opening to wordcloud
 function showfirstcloud() {
   console.log("showfirstcloud");
@@ -42,14 +47,13 @@ function showfirstcloud() {
      var curWord = firstCloudList[i]; 
      curWord.style.opacity = "0%"; 
      curWord.classList.add('fadein'); 
-     var delay = Math.random()*10; 
+     var delay = Math.random()*12; 
      curWord.style.animationDelay = delay + "s"; 
      curWord.style.animationFillMode = "forwards"; 
-    console.log('wordsfadein');
   }
-
+  setTimeout(fadeinbutton, 9500);
   word.style.display = 'none';
   openingcontainer.style.display = 'none';
-  button.style.visibility = 'visible';
+  //button.style.visibility = 'visible';
   firstcloudcontainer.style.display = 'block';
 }
